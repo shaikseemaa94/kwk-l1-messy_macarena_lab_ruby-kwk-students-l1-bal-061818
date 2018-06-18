466a4b8b72b4e8c0cbe924512950d393a15ef6fe
shaikseemaa94 <seema.shaik94@gmail.com>
2018-06-18 19:47:19 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -4,7 +4,7 @@ puts "How To Do The Macarena:"
 # Fix The Dance Instructions Below
 # Every line should have one instruction using puts.
 # The first instruction is correct. 
-
+def Macarena
 # Step 1: GOOD
 puts "Step 1: Right arm out in front of you, palm facing down."
 
@@ -28,3 +28,6 @@ puts "Step 7: Bring your right hand up and around to the back of your head"
 
 # Step 8: FIXME
 puts "Step 8: Bring your left hand up and around to the back of your head"
+end 
+puts "Turn to the left"
+Macarena
